076e4f73690028170c8bac03316d191b01307f5a
minggLu <mylu@bu.edu>
2016-02-04 12:21:53 -0500

    changes base on liberty

--- devstack-k2k/modify_rcfile.sh
+++ devstack-k2k/modify_rcfile.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 USER_ID=$(grep "USER ID" /home/ubuntu/admin | awk -F' = ' '{print $2}')
-PROJECT_ID=$(grep "Tenant ID" /home/ubuntu/admin | awk -F' = ' '{print $2}')
+PROJECT_ID=$(grep "project ID" /home/ubuntu/admin | awk -F' = ' '{print $2}')
 
 sed -i "/USER ID/a export OS_USER_ID=${USER_ID}" /home/ubuntu/admin
 sed -i "/Tenant ID/a export OS_PROJECT_ID=${PROJECT_ID}" /home/ubuntu/admin
